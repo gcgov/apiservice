8467a297fb85e39ed9b89b603bb015308121f428
Andrew Sauder <andrew@sauder.email>
2024-02-09 11:51:05 -0500

    fix type of getAccessTokenFn

--- src/ApiConfig.ts
+++ src/ApiConfig.ts
@@ -2,12 +2,12 @@ class ApiConfig {
     public baseUrl:string
     public baseUrlParams:string
     public useAuthentication:boolean
-    public getAccessTokenFn:Function
+    public getAccessTokenFn:()=>Promise<string>
 
-	constructor( baseUrl:string, baseUrlParams:string, useAuthentication:boolean, getAccessTokenFn=async()=>{} ) {
+	constructor( baseUrl:string, baseUrlParams:string, useAuthentication:boolean, getAccessTokenFn=async():Promise<string>=>{return ''} ) {
 		this.baseUrl = baseUrl
 		this.baseUrlParams = baseUrlParams
-		this.useAuthentication = useAuthentication //bool
+		this.useAuthentication = useAuthentication
 		this.getAccessTokenFn = getAccessTokenFn //async function
 	}
 }
